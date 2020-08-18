edb30508a9e750ab91041fc4e3f09f77c2413238
Suhara Vithanage <suha.lyoko@hotmail.com>
2020-08-18 20:12:23 +0530

    changes in room routes

--- routes/building.routes.js
+++ routes/building.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const buildingController = require('../controllers/building.controller.js');
+const buildingController = require('../controllers/building.controller');
 
 router.post('/buildings', buildingController.addBuilding);
 router.get('/buildings', buildingController.getAllBuildings);
 router.get('/buildings/:id', buildingController.viewBuilding);
+router.put('/buildings/:id', buildingController.updateBuilding);
+router.delete('/buildings/:id', buildingController.deleteBuilding);
 
 module.exports = router;
