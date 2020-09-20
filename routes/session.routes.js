ee96252f2c982435a3b2e105c112a227fa6aca69
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-09-20 14:16:34 +0530

    Created Sessions

--- routes/session.routes.js
+++ routes/session.routes.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const sessionController = require('../controllers/session.controller');
+const router = require("express").Router();
+const sessionController = require("../controllers/session.controller");
 
-router.post('/sessions', sessionController.addSession);
-router.get('/sessions', sessionController.viewSessions);
-router.get('/sessions/:id', sessionController.viewSessionsById);
-router.put('/sessions/:id', sessionController.updateSessionById);
-router.delete('/sessions/:id', sessionController.deleteSessionById);
+router.post("/sessions", sessionController.addSession);
+router.get("/sessions", sessionController.viewSessions);
+router.get("/sessions/:id", sessionController.viewSessionsById);
+router.put("/sessions/:id", sessionController.updateSessionById);
+router.delete("/sessions/:id", sessionController.deleteSessionById);
 
 module.exports = router;
